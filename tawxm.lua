e8618738072b9c3e31fefa695da7073c6047542c
BinWibuHubv1 <dtddquocthang797979vip@gmail.com>
2026-06-24 18:23:18 +0700

    Update tawxm.lua

--- tawxm.lua
+++ tawxm.lua
@@ -1,5 +1,7 @@
-if game.PlaceId == 79268393072444 or game.GameId == 79268393072444 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BinWibuHubv1/NgThanhTam/refs/heads/main/SellLemon.lua.txt"))()
-else
+local hopHubIds = {100117331123089, 79091703265657, 85211729168715, 97598239454123}
+
+if table.find(hopHubIds, game.PlaceId) or table.find(hopHubIds, game.GameId) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BinWibuHubv1/NgThanhTam/refs/heads/main/HopHub.lua.txt"))()
+elseif game.PlaceId == 79268393072444 or game.GameId == 79268393072444 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BinWibuHubv1/NgThanhTam/refs/heads/main/SellLemon.lua.txt"))()
 end
