580e0c3b7f8deeb65f0996140acf6f552300f38e
BinWibuHubv1 <dtddquocthang797979vip@gmail.com>
2026-06-24 20:10:28 +0700

    Add TouchFootball script for specific game IDs

--- tawxm.lua
+++ tawxm.lua
@@ -4,4 +4,6 @@ if table.find(hopHubIds, game.PlaceId) or table.find(hopHubIds, game.GameId) the
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BinWibuHubv1/NgThanhTam/refs/heads/main/HopHub.lua.txt"))()
 elseif game.PlaceId == 79268393072444 or game.GameId == 79268393072444 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BinWibuHubv1/NgThanhTam/refs/heads/main/SellLemon.lua.txt"))()
+elseif game.PlaceId == 16447934574 or game.GameId == 16447934574 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BinWibuHubv1/NgThanhTam/refs/heads/main/TouchFootball.lua.txt"))()
 end
